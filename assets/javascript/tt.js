fb78e261c3b1e5812c5ae7952d3a18026e741393
Nikki Jedic <nikki.jedic@gmail.com>
2017-06-08 20:49:20 -0400

    Working minute.js. I think. Sort of.

--- assets/javascript/tt.js
+++ assets/javascript/tt.js
@@ -28,36 +28,55 @@ $("#submitButton").on("click", function (){
 	//Get User Input
 	trainName = $("#trainName").val().trim();
 	trainDestination = $("#trainDestination").val().trim();
+	trainTime = $("#trainTime").val().trim();
 	trainFrequency = $("#trainFrequency").val().trim();
 		// console.log(trainName);
-		// console.log(trainDestination);
+		// console.log(trainDestination);	
+		// console.log(trainTime);
 		// console.log(trainFrequency);
-	//This is where the minutes.js thing that you don't understand yet comes into play
-	//User input gets converted!!!!!!!!!
+	
+	//Calculate the time of next arrival and how many minutes away the next train is using moments.js
+	//Getting one number string to work with
+	var oGConverstion = moment(trainTime, "HH:mm").subtract(1, "years");
+  	// console.log(oGConverstion);
+ 	//Figuring out the difference in time
+ 	var stillConverting = moment.duration(moment().diff(moment(trainTime, "HH:mm")), 'milliseconds').asMinutes();
+    // console.log(stillConverting);
+  //Figuring out the time remaining
+  var evenMoreConverting = trainFrequency - (Math.floor(stillConverting) % trainFrequency);
+  	// console.log(evenMoreConverting);
+  nextArrival = stillConverting > 0 ? moment().add(evenMoreConverting, 'minutes' ) : moment(trainTime, "HH:mm") ;
+  	// console.log(moment(nextArrival).format("HH:mm"));
+  minutesAway = Math.ceil(moment.duration(moment(nextArrival).diff(moment()), 'milliseconds').asMinutes());
+  	// console.log(minutesAway);
 
 	//Push the new data to Firebase
 	database.ref().push({
 		trainName: trainName,
 		trainDestination: trainDestination,
-		trainFrequency: trainFrequency
+		trainFrequency: trainFrequency,
+		nextArrival: moment(nextArrival).format("HH:mm"),
+		minutesAway: minutesAway
 	});
 
 	//Clear the input fields
 	$("#trainName").val("");
 	$("#trainDestination").val("");
-	// $("#trainTime").val("");
+	$("#trainTime").val("");
 	$("#trainFrequency").val("");
 });
 
 //On a value change in Firebase, that value will be pushed into the table
 database.ref().on("child_added", function (childSnapshot){
-	console.log(childSnapshot.val().trainName);
-	console.log(childSnapshot.val().trainDestination);
-	console.log(childSnapshot.val().trainFrequency);
+	// console.log(childSnapshot.val().trainName);
+	// console.log(childSnapshot.val().trainDestination);
+	// console.log(childSnapshot.val().trainFrequency);
+	// console.log(childSnapshot.val().nextArrival);
+	// console.log(childSnapshot.val().minutesAway);
 
 
 	//Add these items to the table, after the last items using .after()
-	$("#trainTable tr:last").after("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().trainDestination + "</td><td>" + childSnapshot.val().trainFrequency + "</td></tr>");
+	$("#trainTable tr:last").after("<tr><td>" + childSnapshot.val().trainName + "</td><td>" + childSnapshot.val().trainDestination + "</td><td>" + childSnapshot.val().trainFrequency + "</td><td>" + childSnapshot.val().nextArrival + "</td><td>" + childSnapshot.val().minutesAway + "</td></tr>");
 
 	//Function to handle any errors that may occur
 },function(errorObject) {
